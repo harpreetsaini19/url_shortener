3f9f1142bf33099f272f5b524578b29ab16076d5
harpreet <hsaini734@gmail.com>
2014-11-23 18:41:58 +0530

    Change in migration of create hits, foreign key is added along with table creation

--- db/migrate/20141123100506_create_hits.rb
+++ db/migrate/20141123100506_create_hits.rb
@@ -2,11 +2,9 @@ class CreateHits < ActiveRecord::Migration
   def change
     create_table :hits do |t|
       t.integer :shorty_id
+      t.foreign_key :shorties, column: 'shorty_id'
 
       t.timestamps
     end
   end
-
-  add_foreign_key('hits', 'shorties', column: 'shorty_id')
-
 end
